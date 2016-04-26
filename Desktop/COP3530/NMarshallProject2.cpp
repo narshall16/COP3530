f15d7196ae0bf1d3da694aeab4d406aa8e18b9a2
narshall16 <narshall16@gmail.com>
2016-04-26 14:26:21 -0400

    Update NMarshallProject2.cpp
    Should be working now

--- Desktop/COP3530/NMarshallProject2.cpp
+++ Desktop/COP3530/NMarshallProject2.cpp
@@ -1,10 +1,9 @@
-
 /*--------------------------
 
 	Project 2
 	COP 3530
 	4/28/16
-
+	
 ---------------------------*/
 
 #include <iostream>
@@ -24,7 +23,7 @@ int minimum(int a, int b, int c)
 int editDistance(string a, string b)
 {
 	int dist[a.length()+1][b.length()+1];
-
+	
 	//character by character analysis of the two words
 	for(int i = 0; i <= a.size(); ++i)
 	{
@@ -65,14 +64,14 @@ void maxIncantaion(vector<int> &seq, vector<int> &pos)
 	int b;
 	int c;
 	vector<int> longest(seq.size());
-
+	
 	if(seq.empty())
 	{
 		return;
 	}
-
+	
 	pos.push_back(0);
-
+	
 	for(int i = 1; i < seq.size(); ++i)
 	{
 		if(seq[pos.back()] < seq[i])
@@ -81,7 +80,7 @@ void maxIncantaion(vector<int> &seq, vector<int> &pos)
 			pos.push_back(i);
 			continue;
 		}
-
+		
 		a = 0;
 		b = pos.size() - 1;
 		while(a < b)
@@ -96,7 +95,7 @@ void maxIncantaion(vector<int> &seq, vector<int> &pos)
 				b = c;
 			}
 		}
-
+		
 		if(seq[i] < seq[pos[a]])
 		{
 			if(a > 0)
@@ -106,12 +105,29 @@ void maxIncantaion(vector<int> &seq, vector<int> &pos)
 			pos[a] = i;
 		}
 	}
-
+	
 	for(a = pos.size(), b = pos.back(); a--; b = longest[b])
 	{
 		pos[a] = b;
 	}
 }
+
+int closerNode(int length[], bool visited[], int realms)
+{
+	int minimum = 999999999;
+	int minimumIndex = 0;
+	
+	for(int i = 0; i < realms; ++i)
+	{
+		if(!visited[i] && length[i] <= minimum)
+		{
+			minimum = length[i];
+			minimumIndex = i;
+		}
+	}
+	return minimumIndex;
+}
+
 
 int main()
 {
@@ -120,67 +136,51 @@ int main()
 	int cost = 0;
 	int changes = 0;
 	int temp = 0;
+	int curr = 0;
 	string input;
 	string start;
 	string finish;
-	int startn, finishn;
 	vector<string> realmNames;
-	vector<bool> reachable;
-
+	
 	//take in number of realms
 	scanf("%d", &realms);
-	//cin >> realms;
-
-	vector<int> magiVals [realms]; // magi values
-	vector<int> incantations; // compare these
-	vector<int> realmSeq [realms]; // realms in order
-	vector<int> seqPos [realms]; // realms to visit in order
+	vector<int> magiVals [realms];
+	vector<int> incantations;
+	vector<int> realmSeq [realms];
+	vector<int> seqPos [realms];
 	int graph[realms][realms];
-
+	int length[realms];
+	bool reachable[realms];
+	
+	
 	for(int i = 0; i < realms; ++i)
 	{
 		cin >> input;
 		//put realms in vector array
 		realmNames.push_back(input);
-		reachable.push_back(false);
-
+		reachable[i] = false;
 		scanf("%d", &magi);
-		//cin >> magi;
 		for(int j = 0; j < magi; ++j)
 		{
 			//put magi costs in order in a 2D array
-
 			scanf("%d", &cost);
-			//cin >> cost;
 			magiVals[i].push_back(cost);
 		}
-
-			int *temp = new int[magi];
-			for(int k = 0; k < magi; ++k)
-			{
-				temp[k] = magiVals[i][k];
-			}
 			realmSeq[i] = magiVals[i];
 			maxIncantaion(realmSeq[i], seqPos[i]);
 			incantations.push_back(seqPos[i].size());
-			delete [] temp;
-
+		
 	}
-
+	
 	cin >> start;
 	cin >> finish;
-
-	//create undirected graph for all realms
+	
+	//create directed graph for all realms
 	for(int i = 0; i < realms; ++i)
 	{
 		for(int j = 0; j < realms; ++j)
 		{
-			//distance from realm to itself is 0
-			if(j == i)
-			{
-				graph[i][j] = 0;
-			}
-			else if(editDistance(realmNames[i],realmNames[j]) <= incantations[i])
+			if((i != j) && (editDistance(realmNames[i],realmNames[j]) <= incantations[i]))
 			{
 				//edge weights are edit distance for the names
 				//no edge created if cost is higher than # of incantations available
@@ -188,117 +188,136 @@ int main()
 			}
 			else
 			{
-				graph[i][j] = -1;
+				graph[i][j] = 0;			
 			}
-			//cout << graph[i][j] << " ";
 		}
-		//cout << endl;
 	}
-
-	int dist[realms];
+	
 	int predecessor[realms];
+	int predecessor2[realms];
 	int begin;
 	int end;
-	int cost1 = 0;
-	int distance = 0;
 	for(int i = 0; i < realms; ++i)
 	{
-	    if(realmNames[i] == start){
-            startn = i;
-        }
-        if(realmNames[i] == finish){
-            finishn = i;
-        }
 		if(realmNames[i].compare(start) == 0)
 		{
 			begin = i;
-			//cout << "start" << i <<endl;
 		}
 		if(realmNames[i].compare(finish) == 0)
 		{
 			end = i;
-			//cout << "end" << i <<endl;
 		}
 	}
-
-
+	
 	//Dijkstras algorithm
-	//reachable is bool vector to determine if finish is reachable
-	//dist and predecessor are arrays as shown in class lecture
-	//need help finishing this algorithms and we're set.
-
-
+	//couldn't find a good way to pass graph[][] to function
+	//just put the method in main twice.
+	
 	for(int i = 0; i < realms; ++i)
 	{
-		//cout << realmNames[i] << endl;
-		for(int j = 0; j < magiVals[i].size(); ++j)
-		{
-			//cout <<  magiVals[i][j] << " ";
-		}
-		//cout << endl;
-		for(int j = 0; j < seqPos[i].size(); ++j)
-		{
-			//cost1 = cost1 + realmSeq[i][seqPos[i][j]];
-		}
-		//cout << endl;
-		//cout << incantations[i] << endl;
+		length[i] = 999999;
+		predecessor[0] = -1;
 	}
-    bool poss = false;
-    for(int i = 0; i < realms; i++)
+	
+	length[begin] = 0;
+	
+	for(int i = 0; i < (realms - 1); ++i)
 	{
-		for(int j = realms-1; j >= 0; j--)
+		int shortest = closerNode(length, reachable, realms);
+		reachable[shortest] = true;
+		
+		for(int j = 0; j < realms; ++j)
 		{
-		    if ( i == realms-1){
-                poss = true;
-                break;
-            }
-		    //cout << incantations[i] << "and " << graph[i][j] << endl;
-		   if(graph[i][j] > 0 && graph[i][j] <= incantations[i]){
-            incantations[i] = graph[i][j];
-            distance = distance + graph[i][j];
-
-            //cout << "incantations " << incantations[i] << endl;
-            //cout << "distance " << distance << endl;
-            int temp = incantations[i];
-            int z =0;
-            //cout << "i is this " << i << endl;
-            //cout << "cost before " << cost1 <<endl;
-            while(temp>0){
-                cost1 = cost1 + realmSeq[i][seqPos[i][z]];
-               // cout << "this is seqPos " << realmSeq[i][seqPos[i][z]] << endl;
-                //cout << "this is temp" << temp<<endl;
-                temp = temp - 1;
-                z++;
-
-            }
-            //cout << "cost " << cost1 << endl;
-            i = j-1;
-            //cout <<"j is " << j <<endl;
-            //cout <<"i is " << i <<endl;
-            break;
-		   }
-		}
-		if (distance == 0){
-            //   cout << "IMPOSSIBLE" << endl;
-            break;
-		}
-		if(i == realms-1){
-
-                //cout <<"i is this " << i << endl;
-                //cout <<"break" <<endl;
-            break;
+			if(!reachable[j] && graph[shortest][j] && length[shortest] != 999999
+					&& (length[shortest] + graph[shortest][j] < length[j]))
+					{
+						length[j] = length[shortest] + graph[shortest][j];
+						predecessor[j] = shortest;
+					}
 		}
 	}
-	if(poss == true){
-    cout << distance << " " << cost1 << endl;
+	
+	//counts path backwards from destination to start
+	curr = end;
+	cost = 0;
+	while(predecessor[curr] != -1)
+	{
+		//edge weight
+		temp = graph[predecessor[curr]][curr];
+		for(int i = 0; i < temp; ++i)
+		{
+			//counts the cost in each realm
+			changes = predecessor[curr];
+			cost += magiVals[changes][seqPos[changes][i]];
+		}
+		curr = predecessor[curr];
 	}
-	else{
-        cout << "IMPOSSIBLE" << endl;
+	
+	if(length[end] == 999999)
+	{
+		cout << "IMPOSSIBLE" << endl;
 	}
-	//cout << editDistance("sunday","saturday");
+	else
+	{
+		cout << length[end] << " " << cost << endl;
+	}
 
+	
+	
+	changes = end;
+	end = begin;
+	begin = changes;
+	//way back
+	for(int i = 0; i < realms; ++i)
+	{
+		length[i] = 999999;
+		predecessor2[0] = -1;
+		reachable[i] = false;
+	}
+	
+	length[begin] = 0;
+	
+	for(int i = 0; i < (realms - 1); ++i)
+	{
+		int shortest = closerNode(length, reachable, realms);
+		reachable[shortest] = true;
+		
+		for(int j = 0; j < realms; ++j)
+		{
+			if(!reachable[j] && graph[shortest][j] && length[shortest] != 999999
+					&& (length[shortest] + graph[shortest][j] < length[j]))
+					{
+						length[j] = length[shortest] + graph[shortest][j];
+						predecessor2[j] = shortest;
+					}
+		}
+	}
+	
+	//counts path from finish to start
+	curr = end;
+	cost = 0;
+	while(curr != begin)
+	{
+		//edge weight
+		temp = graph[predecessor2[curr]][curr];
+		for(int i = 0; i < temp; ++i)
+		{
+			//counts the cost in each realm
+			changes = predecessor2[curr];
+			cost += magiVals[changes][seqPos[changes][i]];
+		}
+		curr = predecessor2[curr];
+	}
+	
+	if(length[end] == 999999)
+	{
+		cout << "IMPOSSIBLE" << endl;
+	}
+	else
+	{
+		cout << length[end] << " " << cost << endl;
+	}
+		
+	
 	return 0;
 }
-
-
-
